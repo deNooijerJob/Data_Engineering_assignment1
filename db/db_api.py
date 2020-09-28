df0a526d0f571e36aa41d61010b0864c5108d7cb
Job de Nooijer <j.n.d.nooijer@student.tue.nl>
2020-09-28 16:40:40 +0200

    setup of db api + test

---
+++ db/db_api.py
@@ -0,0 +1,10 @@
+from flask import Flask, json, Request, Response
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/db/ping', methods=['POST'])
+def ping():
+    return json.dumps({"message": "alive"}, sort_keys=False, indent=4), 200
+
+app.run(host='0, 0, 0, 0', port=5000)
