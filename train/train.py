5d7bfa4c5b2518875dcf5ee017aff07e5d8c129c
deNooijerJob <j.n.d.nooijer@student.tue.nl>
2020-10-03 08:56:34 +0000

    edit train.py

--- train/train.py
+++ train/train.py
@@ -9,8 +9,8 @@ from simpletransformers.question_answering import QuestionAnsweringModel
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('train/<model>', methods=['POST'])
-def train_model(model):
+@app.route('/train/<model_name>', methods=['POST'])
+def train_model(model_name):
     api = os.environ['DB_API']
     #req = requests.get(api)
     #data = req.json()
@@ -18,7 +18,7 @@ def train_model(model):
     # train_data = pd.DataFrame.from_dict(data)
     #train_data = [item for topic in train_data['data'] for item in topic['paragraphs']]
     #train_data = sample(train_data, 100)
-    if model == "bert":
+    if model_name == "bert":
         train_args = {
             'learning_rate': 3e-5,
             'num_train_epochs': 2,
@@ -33,10 +33,11 @@ def train_model(model):
         model = QuestionAnsweringModel('bert', "deepset/bert-large-uncased-whole-word-masking-squad2", args=train_args,
                                        use_cuda=False)
         #model.train_model(train_data)
-        model.save_model(os.environ['MODEL_REPO'] + '/' + model, model=model.model)
+	path = os.environ['MODEL_REPO'] + "/" + model_name
+        model.save_model(path, model=model.model)
 
 
-    return json.dumps({'message': ' training has started on path ' + model}, sort_keys=False, indent=4), 200
+    return json.dumps({'message': ' training has started on model ' + model_name}, sort_keys=False, indent=4), 200
 
 
 
