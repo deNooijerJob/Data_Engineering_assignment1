f275a57ca173bded61aa263e3f8ba8b1f3548e14
Job de Nooijer <j.n.d.nooijer@student.tue.nl>
2020-10-03 11:15:06 +0200

    fix some bs vi issues 2

--- train/train.py
+++ train/train.py
@@ -33,7 +33,7 @@ def train_model(model_name):
         model = QuestionAnsweringModel('bert', "deepset/bert-large-uncased-whole-word-masking-squad2", args=train_args,
                                        use_cuda=False)
         #model.train_model(train_data)
-	path = os.environ['MODEL_REPO'] + "/" + model_name
+	    path = os.environ['MODEL_REPO'] + "/" + model_name
         model.save_model(path, model=model.model)
 
 
