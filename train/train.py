a13ecff0b40df1087b9f9572c0662709291f587c
Job de Nooijer <j.n.d.nooijer@student.tue.nl>
2020-10-06 17:10:07 +0200

    test

--- train/train.py
+++ train/train.py
@@ -20,7 +20,8 @@ def train_model(model_name):
     train_data = []
     for i in range(0, len(questions)):
         result = {}
-        result['context'] = contexts[questions[i][2]-1][1]
+        #result['context'] = contexts[questions[i][2]-1][1]
+        result['context'] = "Beyonc\u00e9 Giselle Knowles-Carter (/bi\u02d0\u02c8j\u0252nse\u026a/ bee-YON-say) (born September 4, 1981) is an American singer, songwriter, record producer and actress. Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyonc\u00e9's debut album, Dangerously in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot 100 number-one singles \"Crazy in Love\" and \"Baby Boy\"."
         qas = []
         instance = {}
         instance['id'] = str(questions[i][5])
