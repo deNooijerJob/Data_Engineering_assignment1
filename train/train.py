563975a8533fe0a260200a523886b10d9db872c3
Martijn <70754427+mscheve@users.noreply.github.com>
2020-10-06 22:55:08 +0200

    Update train.py

--- train/train.py
+++ train/train.py
@@ -31,7 +31,7 @@ def train_model(model_name):
             'gradient_accumulation_steps': 8,
         }
 
-        model = QuestionAnsweringModel('bert', "deepset/bert-large-uncased-whole-word-masking-squad2", args=train_args,
+        model = QuestionAnsweringModel('bert', "mrm8488/bert-tiny-5-finetuned-squadv2", args=train_args,
                                        use_cuda=False)
         # model.train_model(train_data)
         model.save_model(path, model=model.model)
